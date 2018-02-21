bb929706bca01c8681ad150f128c7c1fff9d9f83
alexisrobin <alexisrobin.bonjour@gmail.com>
2018-02-21 16:19:32 +0100

    :sparkles: Adding elasticsearch data import

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,11 +7,51 @@ var esClient = new elasticsearch.Client({
   log: 'error'
 });
 
+esClient.indices.delete({
+  index: '911',
+}, (err, resp) => {
+  esClient.indices.create({ index: '911' }, (err, resp) => {
+    if (err) console.trace(err.message);
+  });
+});
+
+let calls = [];
+
 fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
-      // TODO extract one line from CSV
+      calls.push({
+        title: data.title.split(": ")[1],
+        type: data.title.split(": ")[0],
+        lat: data.lat,
+        lng: data.lng,
+        desc: data.desc,
+        zip: data.zip,
+        timestamp: data.timeStamp,
+        twp: data.twp,
+        addr: data.addr
+      })
     })
     .on('end', () => {
-      // TODO insert data to ES
+      esClient.bulk(createBulkInsertQuery(calls), (err, resp) => {
+        if (err) console.trace(err.message);
+        else console.log(`Inserted ${resp.items.length} calls`);
+        esClient.close();
+      });
     });
+
+function createBulkInsertQuery(calls) {
+  const body = calls.reduce((acc, call) => {
+    const { title, type, lat, lng, desc, zip, timestamp, twp, addr } = call;
+    acc.push({
+      index:
+        { _index: '911',
+          _type: 'call'
+        }
+    })
+    acc.push({ title, type, lat, lng, desc, zip, timestamp, twp, addr })
+    return acc
+  }, []);
+
+  return { body };
+}
